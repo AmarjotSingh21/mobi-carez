634319aa158a8ab0100d923b8e58adbe757ffcb1
Prince <amarjot.singh070@gmail.com>
2020-07-18 16:53:19 +0530

    Added FAQ

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
