4627f61a2d679f56252d0dc8d82da52f4e8fba3f
Prince <amarjot.singh070@gmail.com>
2020-07-28 15:48:43 +0530

    Added Read more toggle

--- static/script.js
+++ static/script.js
@@ -14,6 +14,27 @@ for (let i = 0; i < faqElements.length; i++) {
   });
 }
 
+let readElements = document.getElementsByClassName("read-toggle");
+console.log(readElements);
+for (let i = 0; i < readElements.length; i++) {
+  readElements[i].addEventListener("click", (el) => {
+    let element = el.target;
+    let parent = element.parentNode;
+    console.log(parent);
+    if (parent.classList.contains("read-less")) {
+      parent.classList.remove("read-less");
+      parent.classList.add("read-more");
+      element.innerHTML =
+        'READ LESS <i class="fa fa-caret-left ml-1" aria-hidden="true"></i>';
+    } else if (parent.classList.contains("read-more")) {
+      parent.classList.remove("read-more");
+      parent.classList.add("read-less");
+      element.innerHTML =
+        'READ MORE <i class="fa fa-caret-right ml-1" aria-hidden="true"></i>';
+    }
+  });
+}
+
 function removeClassFromElements(className, elements, exclude) {
   for (let i = 0; i < elements.length; i++) {
     if (elements[i] != exclude) {
