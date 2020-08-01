ffc91dfbf9ff492df7ca3cbbfdd46d2a57526b45
Prince <amarjot.singh070@gmail.com>
2020-08-01 14:51:44 +0530

    Added font-awesome

--- mobi_carez/settings.py
+++ mobi_carez/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'w^7u8r&aay+5g=qhyo#+srjvtyhsop^+auh$_locbv(l$&jyku'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
