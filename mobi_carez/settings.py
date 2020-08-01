5911d9783784d311972dacd70fc76770812a5c37
Prince <amarjot.singh070@gmail.com>
2020-08-01 15:33:03 +0530

    Added Env Vars

--- mobi_carez/settings.py
+++ mobi_carez/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w^7u8r&aay+5g=qhyo#+srjvtyhsop^+auh$_locbv(l$&jyku'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://mobi-carez.herokuapp.com/']
 
